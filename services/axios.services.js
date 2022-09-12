7abc9cc2ea26d18f046c0a36aa992e7fed24f52e
Carlos Vallejo <carlosvallejo.webmaster@gmail.com>
2022-09-12 17:51:28 +0200

    create routes, services, view jobs and view details-job

--- services/axios.services.js
+++ services/axios.services.js
@@ -3,21 +3,25 @@ const axios = require('axios');
 class AxiosJuniors {
     constructor() {
         this.axios = axios.create({
-            baseURL: 'https://arbeitnow.com/api'
+            baseURL: 'https://www.reed.co.uk/api/1.0',
+            auth: {
+                username: '33b0c34b-163f-4d0d-b895-320394c3e0fa',
+                password: ''
+            }
         });
     }
     getJobs() {
-        return this.axios.get('/job-board-api').then((res) => res.data);
+        return this.axios.get('/search').then((res) => res.data);
     }
 
-    getJob(slug) {
-        return this.axios.get(`/jobs/${slug}`).then((res) => res.data);
+    getJob(jobId) {
+        return this.axios.get(`/jobs/${jobId}`).then((res) => res.data);
     }
 
-    createJob({ name, weapon, occupation, debt }) {
+    createJob({ jobTitle, employedName, locationName, salryType, minimunSalary, maximunSalary, currency, fullTime, partTime, contractType, jobDescription, jobUrl }) {
 
         return this.axios
-            .post('/Jobs', { name, weapon, occupation, debt })
+            .post('/Jobs', { jobTitle, employedName, locationName, salaryType, minimunSalary, maximunSalary, currency, fullTime, partTime, contractType, jobDescription, jobUrl })
             .then((res) => res.data);
     }
 
